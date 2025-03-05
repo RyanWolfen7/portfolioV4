930b2c863b71e2b9d12ffb40e181aa44b5ce4631
Ryan A Clark <42004370+RyanWolfen7@users.noreply.github.com>
2025-03-05 11:38:29 -0600

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import navTabs from "./models/navTabs";
 import ContentSectionSelector from "./views/home/content/_ContentSectionSelector";
 
 const host = process.env.HOST || '0.0.0.0'; // Default to '0.0.0.0' for Vercel
-const port = process.env.PORT || 3000; // Default to 3000
+const port = parseInt(process.env.PORT || '3000', 10); // Use PORT environment variable or default to 3000
 
 const app = new Elysia()
   .use(html())
