8e0734e03e35dea6b11034f10d76d5786d9006cf
Ryan A Clark <42004370+RyanWolfen7@users.noreply.github.com>
2025-03-05 11:53:29 -0600

    updated

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,31 @@
-/**
- * This file is the entrypoint for all Vercel Functions.
- */
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import * as elements from "typed-html";
+import Home from "../src/views/home/Home";
+import NavSelect from "../src/views/home/nav/NavSelect";
+import Content from "../src/views/home/Content";
+import navTabs from "../src/models/navTabs";
 
-import '../src/env'; // Validate env vars
+import ContentSectionSelector from "../src/views/home/content/_ContentSectionSelector";
 
-import app from '../src/index';
+const host = process.env.HOST || '0.0.0.0'; // Default to '0.0.0.0' for Vercel
+const port = parseInt(process.env.PORT || '3000', 10); // Use PORT environment variable or default to 3000
 
-export const config = { runtime: 'edge' };
+const app = new Elysia()
+  .use(html())
+  .state('navTabs', [...navTabs])
+  .state('navTabLoadCounter', 0)
+  .state('initialLoad', false)
+  .get("/", Home)
+  .post("/nav/:target", NavSelect)
+  .get("/content", Content)
+  .post("/content/:target", ContentSectionSelector)
+  .get("/resume", () => Bun.file("./public/ShortRyanClark.FullStack.2024.pdf"))
+  .get("/styles.css", () => Bun.file("./tailwind-gen/styles.css"))
+  .listen({ port, hostname: host });
 
-export default async function handler(request: Request) {
-  return app.fetch(request);
-}
+console.log(
+  `Your application is running at http://${app.server?.hostname}:${app.server?.port}`
+);
+
+export default app;
