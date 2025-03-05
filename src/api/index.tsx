efe8761d6dca3aa90d3ed2019e54a077d353296b
Ryan A Clark <42004370+RyanWolfen7@users.noreply.github.com>
2025-03-05 10:58:21 -0600

    added app/index for vercel

---
+++ src/api/index.tsx
@@ -0,0 +1,31 @@
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import * as elements from "typed-html";
+import Home from "../views/home/Home";
+import NavSelect from "../views/home/nav/NavSelect";
+import Content from "../views/home/Content";
+import navTabs from "../models/navTabs";
+
+import ContentSectionSelector from "../views/home/content/_ContentSectionSelector";
+
+const host = process.env.HOST || '0.0.0.0'; // Default to '0.0.0.0' for Vercel
+const port = process.env.PORT || 3000; // Default to 3000
+
+const app = new Elysia()
+  .use(html())
+  .state('navTabs', [...navTabs])
+  .state('navTabLoadCounter', 0)
+  .state('initialLoad', false)
+  .get("/", Home)
+  .post("/nav/:target", NavSelect)
+  .get("/content", Content)
+  .post("/content/:target", ContentSectionSelector)
+  .get("/resume", () => Bun.file("./public/ShortRyanClark.FullStack.2024.pdf"))
+  .get("/styles.css", () => Bun.file("./tailwind-gen/styles.css"))
+  .listen(port);
+
+console.log(
+  `Your application is running at http://${app.server?.hostname}:${app.server?.port}`
+);
+
+export default app;
